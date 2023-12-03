995d01086240f5e4644facb4769321b37e914a83
sasha <a.s.gorokhova@yandex.ru>
2023-12-03 20:00:44 +0300

    added .h file for Element class

---
+++ Element.h
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <sstream>
+#include <vector>
+#include <string>
+using namespace std;
+
+enum class ElementType { E, C, R, L, I };
+
+class Element {
+private:
+    ElementType type;
+    int startNode;
+    int endNode;
+    double value;
+    std::string name;
+
+public:
+    Element(ElementType elementType, int start, int end, double val, const std::string& elemName)
+        : type(elementType), startNode(start), endNode(end), value(val), name(elemName) {}
+
+    ElementType getType() const {
+        return type;
+    }
+
+
+    int getStartNode() const {
+        return startNode;
+    }
+
+
+    int getEndNode() const {
+        return endNode;
+    }
+
+
+    double getValue() const {
+        return value;
+    }
+
+
+    std::string getName() const {
+        return name;
+    }
+
+};
