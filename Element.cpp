4241e6a8c3cc0cd2be37841b28f3c5907d3fa40b
sasha <a.s.gorokhova@yandex.ru>
2023-12-03 20:00:07 +0300

    check if .h file works correctly

--- Element.cpp
+++ Element.cpp
@@ -2,48 +2,10 @@
 #include <sstream>
 #include <vector>
 #include <string>
+#include "Element.h"
 using namespace std;
 
-enum class ElementType { E, C, R, L, I };
-
-class Element {
-private:
-    ElementType type;
-    int startNode;
-    int endNode;
-    double value;
-    std::string name;
-
-public:
-    Element(ElementType elementType, int start, int end, double val, const std::string& elemName)
-        : type(elementType), startNode(start), endNode(end), value(val), name(elemName) {}
-
-    ElementType getType() const {
-        return type;
-    }
-
-
-    int getStartNode() const {
-        return startNode;
-    }
-
-
-    int getEndNode() const {
-        return endNode;
-    }
-
-
-    double getValue() const {
-        return value;
-    }
-
-
-    std::string getName() const {
-        return name;
-    }
-
-};
-
+// check if works
 int main() {
     vector<Element> elements;
 
